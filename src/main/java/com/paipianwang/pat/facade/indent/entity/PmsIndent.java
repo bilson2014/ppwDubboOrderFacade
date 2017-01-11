c2e3ba319bf638843e513d58e3a0a41b47c2825c
wangliucheng <itliucheng@126.com>
2017-01-11 12:57:39 +0800

    init

---
+++ src/main/java/com/paipianwang/pat/facade/indent/entity/PmsIndent.java
@@ -0,0 +1,15 @@
+package com.paipianwang.pat.facade.indent.entity;
+
+import com.paipianwang.pat.common.entity.BaseEntity;
+
+/**
+ * 订单业务实体类
+ * @author Jack
+ * @version 1.0
+ *
+ */
+public class PmsIndent extends BaseEntity {
+
+	private static final long serialVersionUID = -3318289705685043573L;
+
+}
---
+++ src/main/java/com/paipianwang/pat/facade/indent/service/PmsIndentFacade.java
@@ -0,0 +1,11 @@
+package com.paipianwang.pat.facade.indent.service;
+
+/**
+ * 订单Dubbo服务接口
+ * @author Jack
+ * @version 1.0
+ *
+ */
+public interface PmsIndentFacade {
+
+}

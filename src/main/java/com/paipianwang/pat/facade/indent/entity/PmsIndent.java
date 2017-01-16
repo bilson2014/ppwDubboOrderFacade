2ad2c43d96095fa85419484b4cffbe95cadc1432
wangliucheng <itliucheng@126.com>
2017-01-16 12:27:34 +0800

    update

--- src/main/java/com/paipianwang/pat/facade/indent/entity/PmsIndent.java
+++ src/main/java/com/paipianwang/pat/facade/indent/entity/PmsIndent.java
@@ -9,7 +9,178 @@ import com.paipianwang.pat.common.entity.BaseEntity;
  *
  */
 public class PmsIndent extends BaseEntity {
-
 	private static final long serialVersionUID = -3318289705685043573L;
+	
+	/** 订单名称 **/
+	private String indentName = null; 
+	
+	/** 订单编号 **/
+	private String indentNum = null; 
+	
+	/** 0:新订单 1:处理中 2:完成 3:停滞 **/
+	private int indentType;
+	
+	private Long serviceId = null;
+	
+	private String orderDate = null;
+	
+	private Long userId = null;
+	
+	/** 订单金额 **/
+	private double indentPrice = 0.0;
+	
+	/** 产品ID **/
+	private long productId = 0l;
+	
+	/** 所属团队ID **/
+	private long teamId = 0l;
+	
+	/** 视频分钟数 **/
+	private long second = 0l;
+	
+	/** 订单-联系电话 **/
+	private String indent_tele = null;
+	
+	/** 订单备注**/
+	private String indent_recomment = null;
+	
+	/** CRM备注**/
+	private String indent_description = null;
+	
+	/** 分销人唯一编号 **/
+	private String salesmanUniqueId = null; 
+	
+	/** 订单给业务人员发短信 **/
+	private Boolean sendToStaff = true;
+	
+	/** 订单给客户发短信 **/
+	private Boolean sendToUser = true;
+	
+	public String getIndentName() {
+		return indentName;
+	}
 
+	public void setIndentName(String indentName) {
+		this.indentName = indentName;
+	}
+
+	public String getIndentNum() {
+		return indentNum;
+	}
+
+	public void setIndentNum(String indentNum) {
+		this.indentNum = indentNum;
+	}
+
+	public int getIndentType() {
+		return indentType;
+	}
+
+	public void setIndentType(int indentType) {
+		this.indentType = indentType;
+	}
+
+	public Long getServiceId() {
+		return serviceId;
+	}
+
+	public void setServiceId(Long serviceId) {
+		this.serviceId = serviceId;
+	}
+
+	public String getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public double getIndentPrice() {
+		return indentPrice;
+	}
+
+	public void setIndentPrice(double indentPrice) {
+		this.indentPrice = indentPrice;
+	}
+
+	public long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+
+	public long getTeamId() {
+		return teamId;
+	}
+
+	public void setTeamId(long teamId) {
+		this.teamId = teamId;
+	}
+
+	public long getSecond() {
+		return second;
+	}
+
+	public void setSecond(long second) {
+		this.second = second;
+	}
+
+	public String getIndent_tele() {
+		return indent_tele;
+	}
+
+	public void setIndent_tele(String indent_tele) {
+		this.indent_tele = indent_tele;
+	}
+
+	public String getIndent_recomment() {
+		return indent_recomment;
+	}
+
+	public void setIndent_recomment(String indent_recomment) {
+		this.indent_recomment = indent_recomment;
+	}
+
+	public String getIndent_description() {
+		return indent_description;
+	}
+
+	public void setIndent_description(String indent_description) {
+		this.indent_description = indent_description;
+	}
+
+	public String getSalesmanUniqueId() {
+		return salesmanUniqueId;
+	}
+
+	public void setSalesmanUniqueId(String salesmanUniqueId) {
+		this.salesmanUniqueId = salesmanUniqueId;
+	}
+
+	public Boolean getSendToStaff() {
+		return sendToStaff;
+	}
+
+	public void setSendToStaff(Boolean sendToStaff) {
+		this.sendToStaff = sendToStaff;
+	}
+
+	public Boolean getSendToUser() {
+		return sendToUser;
+	}
+
+	public void setSendToUser(Boolean sendToUser) {
+		this.sendToUser = sendToUser;
+	}
 }
--- src/main/java/com/paipianwang/pat/facade/indent/service/PmsIndentFacade.java
+++ src/main/java/com/paipianwang/pat/facade/indent/service/PmsIndentFacade.java
@@ -1,5 +1,12 @@
 package com.paipianwang.pat.facade.indent.service;
 
+import java.util.List;
+import java.util.Map;
+
+import com.paipianwang.pat.common.entity.DataGrid;
+import com.paipianwang.pat.common.entity.PageParam;
+import com.paipianwang.pat.facade.indent.entity.PmsIndent;
+
 /**
  * 订单Dubbo服务接口
  * @author Jack
@@ -8,4 +15,54 @@ package com.paipianwang.pat.facade.indent.service;
  */
 public interface PmsIndentFacade {
 
+	/**
+	 * DataGride 查询订单列表(含条件)
+	 */
+	public DataGrid<PmsIndent> listWithPagination(PageParam pageParam, Map<String, Object> paramMap);
+	/**
+	 * 添加一条订单
+	 */
+	public long save(PmsIndent pmsIndent);
+	/**
+	 * 更新一条订单
+	 */
+	public long update(PmsIndent pmsIndent);
+	/**
+	 * 根据ids数组删除订单
+	 */
+	public long delIndentByIds(long[] ids);
+	/**
+	 *  批量修改订单状态
+	 * @param ids ID数组
+	 * @param indentType 订单状态值 0:新订单 1:处理中 2:完成 3:停滞
+	 * @return
+	 */
+	public boolean changeIndentsType(long[] ids,int indentType);
+	/**
+	 * 条件查询list
+	 * @param paramMap 条件
+	 */
+	public List<PmsIndent> listWithCondition(Map<String, Object> paramMap);
+	/**
+	 * 前端下单
+	 */
+	public boolean saveOrder(final PmsIndent indent);
+	/**
+	 * 检测新订单个数
+	 */
+	public long checkStatus(final int status);
+	/**
+	 * 更新成本计算器订单
+	 */
+	public long updateForCalculate(PmsIndent indent);
+	/**
+	 * 查询某人订单总数
+	 * @param salesmanUniqueId 某人唯一标识
+	 */
+	public long countBySalesmanUniqueId(String salesmanUniqueId);
+	/**
+	 * 查询某人订单总金额
+	 * @param salesmanUniqueId 某人唯一标识
+	 */
+	public Double sumPriceBySalesmanUniqueId(String salesmanUniqueId);
 }

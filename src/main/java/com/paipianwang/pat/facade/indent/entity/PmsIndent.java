5ba2c7ef72f28cc9b5b12acfc1b4fba876374e58
laowang <1300792971@qq.com>
2017-05-17 18:56:57 +0800

    lw

--- src/main/java/com/paipianwang/pat/facade/indent/entity/PmsIndent.java
+++ src/main/java/com/paipianwang/pat/facade/indent/entity/PmsIndent.java
@@ -1,88 +1,117 @@
 package com.paipianwang.pat.facade.indent.entity;
 
 import com.paipianwang.pat.common.entity.BaseEntity;
+import com.paipianwang.pat.facade.user.entity.PmsUser;
 
 /**
  * 订单业务实体类
+ * 
  * @author Jack
  * @version 1.0
  *
  */
 public class PmsIndent extends BaseEntity {
 	private static final long serialVersionUID = -3318289705685043573L;
-	
+
 	/** 订单名称 **/
-	private String indentName = null; 
-	
+	private String indentName = null;
+
 	/** 订单编号 **/
-	private String indentNum = null; 
-	
+	private String indentNum = null;
+
 	/** 0:新订单 1:处理中 2:完成 3:停滞 **/
 	private int indentType;
-	
+
 	private Long serviceId = null;
-	
+
 	private String orderDate = null;
-	
-	private Long userId = null;
-	
+
 	/** 订单金额 **/
 	private double indentPrice = 0.0;
-	
+
 	/** 产品ID **/
 	private long productId = 0l;
-	
+
 	/** 所属团队ID **/
 	private long teamId = 0l;
-	
+
 	/** 视频分钟数 **/
 	private long second = 0l;
-	
+
 	/** 订单-联系电话 **/
 	private String indent_tele = null;
-	
-	/** 订单备注**/
+
+	/** 订单备注 **/
 	private String indent_recomment = null;
-	
-	/** CRM备注**/
+
+	/** CRM备注 **/
 	private String indent_description = null;
-	
+
 	/** 分销人唯一编号 **/
-	private String salesmanUniqueId = null; 
-	
+	private String salesmanUniqueId = null;
+
 	/** 订单给业务人员发短信 **/
 	private Boolean sendToStaff = true;
-	
+
 	/** 订单给客户发短信 **/
 	private Boolean sendToUser = true;
-	
-	/** 分销人名字**/
+
+	/** 分销人名字 **/
 	private String salesmanName = null;
-	
+
 	private long indentId = 0l; // 订单编号
-	
-	private String service_name = null; 
-	
+
+	private String service_name = null;
+
 	private double service_price = 0.0d;
-	
+
 	private double service_discount = 0.0d;
-	
+
 	private double service_realPrice = 0.0d;
 
 	private String product_name = null;
-	
+
 	private String team_name = null;
-	
+
 	private String user_name = null;
-	
+
 	private String telephone = null;
-	
+
 	private String user_email = null;
-	
-	private long[] ids;//订单数组
-	
+
+	private long[] ids;// 订单数组
+
 	private String token = null;
-	
+
+	/**
+	 * 客服ID
+	 */
+	private Long employeeId;
+
+	/**
+	 * 临时客户信息
+	 */
+
+	private Long userId = null;
+	/**
+	 * 用户公司
+	 */
+	private String userCompany;
+	/**
+	 * 联系人
+	 */
+	private String realName;
+	/**
+	 * 微信
+	 */
+	private String wechat;
+	/**
+	 * 职位
+	 */
+	private Integer position;
+
+	private Long requireId;
+
 	public String getToken() {
 		return token;
 	}
@@ -314,4 +343,90 @@ public class PmsIndent extends BaseEntity {
 	public void setIds(long[] ids) {
 		this.ids = ids;
 	}
+
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getUserCompany() {
+		return userCompany;
+	}
+
+	public void setUserCompany(String userCompany) {
+		this.userCompany = userCompany;
+	}
+
+	public String getRealName() {
+		return realName;
+	}
+
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+
+	public String getWechat() {
+		return wechat;
+	}
+
+	public void setWechat(String wechat) {
+		this.wechat = wechat;
+	}
+
+	public Integer getPosition() {
+		return position;
+	}
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+
+	public Long getRequireId() {
+		return requireId;
+	}
+
+	public void setRequireId(Long requireId) {
+		this.requireId = requireId;
+	}
+
+	/**
+	 * 根据当前订单实例，生成用户实例
+	 * 
+	 * @return
+	 *//*
+	public PmsUser getUser() {
+		PmsUser user = new PmsUser();
+		user.setUserCompany(this.userCompany);
+		user.setRealName(this.realName);
+		user.setWeChat(this.wechat);
+		user.setTelephone(this.telephone);
+		return user;
+	}
+
+	*//**
+	 * 比较订单关联的客户是否有修改
+	 * 
+	 * @param user
+	 * @return
+	 *//*
+	public boolean compareDBUser(PmsUser user) {
+		if (user != null) {
+			if (user.getUserId() == this.userId) {
+				if (user.getUserCompany() != null && user.getUserCompany().equals(this.userCompany)) {
+					if (user.getRealName() != null && user.getRealName().equals(this.realName)) {
+						if (user.getWeChat() == null
+								|| (user.getWeChat() != null && user.getWeChat().equals(this.wechat))) {
+							if (user.getTelephone() != null && user.getTelephone().equals(this.telephone))
+								return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+*/
 }
--- src/main/java/com/paipianwang/pat/facade/indent/service/PmsIndentFacade.java
+++ src/main/java/com/paipianwang/pat/facade/indent/service/PmsIndentFacade.java
@@ -9,6 +9,7 @@ import com.paipianwang.pat.facade.indent.entity.PmsIndent;
 
 /**
  * 订单Dubbo服务接口
+ * 
  * @author Jack
  * @version 1.0
  *
@@ -19,50 +20,77 @@ public interface PmsIndentFacade {
 	 * DataGride 查询订单列表(含条件)
 	 */
 	public DataGrid<PmsIndent> listWithPagination(PageParam pageParam, Map<String, Object> paramMap);
+
 	/**
 	 * 添加一条订单
 	 */
 	public long save(PmsIndent pmsIndent);
+
 	/**
 	 * 更新一条订单
 	 */
 	public long update(PmsIndent pmsIndent);
+
 	/**
 	 * 根据ids数组删除订单
 	 */
 	public long delIndentByIds(long[] ids);
+
 	/**
-	 *  批量修改订单状态
-	 * @param ids ID数组
-	 * @param indentType 订单状态值 0:新订单 1:处理中 2:完成 3:停滞
+	 * 批量修改订单状态
+	 * 
+	 * @param ids
+	 *            ID数组
+	 * @param indentType
+	 *            订单状态值 0:新订单 1:处理中 2:完成 3:停滞
 	 * @return
 	 */
-	public boolean changeIndentsType(long[] ids,int indentType);
+	public boolean changeIndentsType(long[] ids, int indentType);
+
 	/**
 	 * 条件查询list
-	 * @param paramMap 条件
+	 * 
+	 * @param paramMap
+	 *            条件
 	 */
 	public List<PmsIndent> listWithCondition(Map<String, Object> paramMap);
+
 	/**
 	 * 前端下单
 	 */
 	public boolean saveOrder(final PmsIndent indent);
+
 	/**
 	 * 检测新订单个数
 	 */
 	public long checkStatus(final int status);
+
 	/**
 	 * 更新成本计算器订单
 	 */
 	public long updateForCalculate(PmsIndent indent);
+
 	/**
 	 * 查询某人订单总数
-	 * @param salesmanUniqueId 某人唯一标识
+	 * 
+	 * @param salesmanUniqueId
+	 *            某人唯一标识
 	 */
 	public long countBySalesmanUniqueId(String salesmanUniqueId);
+
 	/**
 	 * 查询某人订单总金额
-	 * @param salesmanUniqueId 某人唯一标识
+	 * 
+	 * @param salesmanUniqueId
+	 *            某人唯一标识
 	 */
 	public Double sumPriceBySalesmanUniqueId(String salesmanUniqueId);
+
+	/**
+	 * 更新订单客服
+	 * @param indent
+	 * @return
+	 */
+	public long updateCustomerService(PmsIndent indent);
+	
 }
